cc1720a4e189147a3f3e61d4a7bc6f597e98de43
Chidi Michael <43938389+chiddy50@users.noreply.github.com>
2024-10-13 23:26:09 +0100

    getCode login testing

--- components/AuthenticationButton.tsx
+++ components/AuthenticationButton.tsx
@@ -261,6 +261,8 @@ const AuthenticationButton = () => {
         localStorage.removeItem("idToken");
         localStorage.removeItem("publicAddress");
         localStorage.removeItem("appPubKey")
+        localStorage.setItem('redirectRoute');
+        
         setProvider(null);
         setLoggedIn(false);
         setOpenLogoutModal(false);
