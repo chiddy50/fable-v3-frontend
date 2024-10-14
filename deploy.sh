8eee7e94b021b4448dc57d1bf4e5d5330ca60b6f
Oladipupo <oladipupo@sproutlyhq.com>
2024-10-14 06:35:57 -0500

    update

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+APP_DIR="/root/fable"
+BACKEND_DIR="$APP_DIR/fable-v3-api/"
+FRONTEND_DIR="$APP_DIR/fable-v3-frontend/"
+BRANCH="devops"
+
+echo "update API code base"
+
+pwd
+
+cd $BACKEND_DIR || exit
+
+echo "Pulling the latest code from branch $BRANCH..."
+git fetch --all
+git reset --hard origin/$BRANCH
+
+echo "refreshing deploymemnt..."
+cd $APP_DIR || exit
+
+
+# Rebuild and start the Docker Compose services
+echo "Rebuilding and starting Docker Compose services..."
+docker-compose up -d --build
+
+echo "waiting for health status"
+sleep 30
+
+echo "Checking Docker services status..."
+docker compose ps --format "table {{.Name}}\t{{.Status}}\t{{.Ports}}"
+
+echo "Deployment completed successfully."
