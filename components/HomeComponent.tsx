edd3fb5f3a862d8675aeb46d37ad9c2a94ca3345
Chidi Michael <43938389+chiddy50@users.noreply.github.com>
2024-10-23 11:03:50 +0100

    Merge conflict resolved

--- components/HomeComponent.tsx
+++ components/HomeComponent.tsx
@@ -314,7 +314,7 @@ const HomeComponent = () => {
                     }
                     
                     {
-                        !loading && publishedStories?.length &&
+                        !loading && publishedStories?.length > 0 &&
                         <div id="published-content">
                             <h1 className="mb-5 text-gray-600 xs:text-3xl sm:text-3xl text-4xl font-bold">
                             Checkout these stories..
