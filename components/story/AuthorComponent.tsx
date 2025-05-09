ec367a57853c34efb8d5175c283362cb1b7d9a92
Chidi Michael <43938389+chiddy50@users.noreply.github.com>
2025-05-09 15:35:58 +0100

    Version 4 changes done

--- components/story/AuthorComponent.tsx
+++ components/story/AuthorComponent.tsx
@@ -81,9 +81,8 @@ const AuthorComponent: React.FC<Props> =  ({ count, name, publishedAt, imageUrl,
                         imageMargin='mr-2'
                     /> */}
                     <img
-                        src={imageUrl ?? `/avatar/default-avatar.png `} 
-                        alt="User avatar"
-            
+                        src={user?.imageUrl ?? imageUrl ?? "/avatar/default-avatar.png"} 
+                        alt="User avatar"            
                         className={`${borderRadius ?? 'rounded-xl'} w-[35px] h-[35px] object-cover mr-2`}
                     />
                     <div className='text-[10px] relative'>
