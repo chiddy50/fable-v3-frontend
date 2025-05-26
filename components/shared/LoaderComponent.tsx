48576f1840c5f3a7768b059dc54050369382df82
Chidi Michael <43938389+chiddy50@users.noreply.github.com>
2025-05-26 23:17:19 +0100

    New beneficial owners tab and page added

--- components/shared/LoaderComponent.tsx
+++ components/shared/LoaderComponent.tsx
@@ -6,7 +6,8 @@ const LoaderComponent = () => {
     return (
         <div className="fixed inset-0 flex items-center justify-center w-screen h-screen bg-white">
             {/* <i className="bx bx-loader bx-spin text-[10rem]"></i> */}
-            <i className='bx bxs-hourglass-top bx-flashing text-[10rem]' ></i>
+            {/* <i className='bx bxs-hourglass-top bx-flashing text-[10rem]' ></i> */}
+            <div className='loader'></div>
         </div>
     )
 }
